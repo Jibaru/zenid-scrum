71a1e836c9bfec97feb97189d5fc18d8cbc4bed5
Ignacio <ignacioruedaboada@hotmail.com>
2021-10-29 14:06:10 -0500

    Creado privilegio

---
+++ src/main/java/com/untels/zenidscrum/modelo/bean/Privilegio.java
@@ -0,0 +1,69 @@
+package com.untels.zenidscrum.modelo.bean;
+
+public class Privilegio {
+
+    private int idPrivilegio;
+    private String nombre;
+    private boolean crear;
+    private boolean actualizar;
+    private boolean listar;
+    private boolean eliminar;
+    private int idRol;
+
+    public int getIdPrivilegio() {
+        return idPrivilegio;
+    }
+
+    public void setIdPrivilegio(int idPrivilegio) {
+        this.idPrivilegio = idPrivilegio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isCrear() {
+        return crear;
+    }
+
+    public void setCrear(boolean crear) {
+        this.crear = crear;
+    }
+
+    public boolean isActualizar() {
+        return actualizar;
+    }
+
+    public void setActualizar(boolean actualizar) {
+        this.actualizar = actualizar;
+    }
+
+    public boolean isListar() {
+        return listar;
+    }
+
+    public void setListar(boolean listar) {
+        this.listar = listar;
+    }
+
+    public boolean isEliminar() {
+        return eliminar;
+    }
+
+    public void setEliminar(boolean eliminar) {
+        this.eliminar = eliminar;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+}
