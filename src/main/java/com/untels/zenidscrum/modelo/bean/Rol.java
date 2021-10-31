424eed73bb781513e48f01bd25cd5b9aac015d96
Ignacio <ignacioruedaboada@hotmail.com>
2021-10-31 17:59:03 -0500

    Agregado habilitado

--- src/main/java/com/untels/zenidscrum/modelo/bean/Rol.java
+++ src/main/java/com/untels/zenidscrum/modelo/bean/Rol.java
@@ -7,6 +7,7 @@ public class Rol {
     private int idRol;
     private String nombre;
     private String descripcion;
+    private boolean habilitado;
     private List<Privilegio> privilegios;
 
     public int getIdRol() {
@@ -31,6 +32,14 @@ public class Rol {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
     }
 
     public List<Privilegio> getPrivilegios() {
