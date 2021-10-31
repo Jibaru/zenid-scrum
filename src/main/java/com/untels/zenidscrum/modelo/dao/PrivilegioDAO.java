446d65a24f0b1ad72b8985a06353bf1acdde8816
Ignacio <ignacioruedaboada@hotmail.com>
2021-10-31 18:00:29 -0500

    Creada interfaz PrivilegioDAO

---
+++ src/main/java/com/untels/zenidscrum/modelo/dao/PrivilegioDAO.java
@@ -0,0 +1,10 @@
+package com.untels.zenidscrum.modelo.dao;
+
+import com.untels.zenidscrum.modelo.bean.Privilegio;
+
+public interface PrivilegioDAO {
+
+    public boolean crear(Privilegio privilegio);
+
+    public boolean modificar(Privilegio privilegio);
+}
