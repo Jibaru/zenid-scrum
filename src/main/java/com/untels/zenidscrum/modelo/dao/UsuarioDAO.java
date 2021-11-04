eba50f9eb729884dbcd41751a8044039af4022de
Ignacio <ignacioruedaboada@hotmail.com>
2021-11-04 09:31:02 -0500

    Agregados metodos crud

--- src/main/java/com/untels/zenidscrum/modelo/dao/UsuarioDAO.java
+++ src/main/java/com/untels/zenidscrum/modelo/dao/UsuarioDAO.java
@@ -1,8 +1,21 @@
 package com.untels.zenidscrum.modelo.dao;
 
 import com.untels.zenidscrum.modelo.bean.Usuario;
+import java.util.List;
 
 public interface UsuarioDAO {
 
     public Usuario obtenerUsuarioPorCorreoElectronico(String correoElectronico);
+
+    public List<Usuario> listarTodos();
+
+    public Usuario obtenerPorId(int idUsuario);
+
+    public boolean crear(Usuario usuario);
+
+    public boolean modificar(Usuario usuario);
+
+    public boolean inhabilitar(int idUsuario);
+
+    public boolean habilitar(int idUsuario);
 }
