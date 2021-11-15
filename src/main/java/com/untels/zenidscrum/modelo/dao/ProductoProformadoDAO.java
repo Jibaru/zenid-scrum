7a011aab4b300e69f9e365140049a44bf973052d
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 15:17:32 -0500

    Creado ProductoProformadoDAO

---
+++ src/main/java/com/untels/zenidscrum/modelo/dao/ProductoProformadoDAO.java
@@ -0,0 +1,8 @@
+package com.untels.zenidscrum.modelo.dao;
+
+import com.untels.zenidscrum.modelo.bean.ProductoProformado;
+
+public interface ProductoProformadoDAO {
+
+    public boolean crear(ProductoProformado p);
+}
