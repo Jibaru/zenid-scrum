341bee698085ec18316522f80fec60faca99ca7e
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 03:14:30 -0500

    Agregado método listarTodos

--- src/main/java/com/untels/zenidscrum/modelo/dao/ProductoDAO.java
+++ src/main/java/com/untels/zenidscrum/modelo/dao/ProductoDAO.java
@@ -6,6 +6,8 @@ import java.util.List;
 public interface ProductoDAO {
 
     public List<Producto> buscar(String termino, String marca, Integer idProveedor);
-    
+
     public List<Producto> buscarEquivalentes(String termino, String marca, Integer idProveedor);
+
+    public List<Producto> listarTodos();
 }
