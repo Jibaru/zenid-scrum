29892257687b46cb49814ba29b0ba8247102bcca
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 00:38:29 -0500

    Cambiado acceso a conexion

--- src/main/java/com/untels/zenidscrum/acceso/datos/SQLConexion.java
+++ src/main/java/com/untels/zenidscrum/acceso/datos/SQLConexion.java
@@ -5,22 +5,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLConexion implements Conexion {
-    
-    private final String URL = "jdbc:mysql://localhost/bd_zenid_scrum?useSSL=false&serverTimezone=UTC&useLegacyDateTimeCode=false";
-    private final String USUARIO = "root";
-    private final String CLAVE = "root";
-   
+
+    private final String HOST = System.getenv("ZENID_SCRUM_BD_HOST");
+    private final String PUERTO = System.getenv("ZENID_SCRUM_BD_PORT");
+    private final String NOMBRE = System.getenv("ZENID_SCRUM_BD_NAME");
+    private final String USUARIO = System.getenv("ZENID_SCRUM_BD_USER");
+    private final String CLAVE = System.getenv("ZENID_SCRUM_BD_PASSWORD");
+    private final String url;
+
+    public SQLConexion() {
+        url = "jdbc:mysql://" + HOST + ":" + PUERTO + "/" + NOMBRE + "?"
+                + "useSSL=false&"
+                + "serverTimezone=UTC&"
+                + "allowPublicKeyRetrieval=true&"
+                + "useLegacyDateTimeCode=false";
+    }
 
     @Override
     public Connection getConnection() {
         Connection cn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            cn = DriverManager.getConnection(URL, USUARIO, CLAVE);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(url, USUARIO, CLAVE);
 
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println(ex.getMessage());
         }
         return cn;
     }
+
 }
