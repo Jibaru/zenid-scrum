6ce0ce379b0ed690a35e3ad765e175525ad78574
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 03:24:22 -0500

    Creado bean Vendedor

---
+++ src/main/java/com/untels/zenidscrum/modelo/bean/Vendedor.java
@@ -0,0 +1,42 @@
+package com.untels.zenidscrum.modelo.bean;
+
+public class Vendedor {
+
+    private int idVendedor;
+    private String nombre;
+    private String correoElectronico;
+    private String telefono;
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
