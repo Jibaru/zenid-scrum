3a653cfa00c56b752d0a9f615e9cf474fef06b26
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 23:35:52 -0500

    Agregado habilitar e inhabilitar

--- src/main/java/com/untels/zenidscrum/modelo/dao/ProveedorDAO.java
+++ src/main/java/com/untels/zenidscrum/modelo/dao/ProveedorDAO.java
@@ -6,11 +6,15 @@ import java.util.List;
 public interface ProveedorDAO {
 
     public List<Proveedor> listarTodos();
-    
-     public Proveedor obtenerPorId(int idProveedor);
+
+    public Proveedor obtenerPorId(int idProveedor);
 
     public boolean crear(Proveedor proveedor);
 
     public boolean modificar(Proveedor proveedor);
-       
+
+    public boolean inhabilitar(int idProveedor);
+
+    public boolean habilitar(int idProveedor);
+
 }
