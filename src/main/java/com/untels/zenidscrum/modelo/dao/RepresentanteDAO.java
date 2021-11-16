3d87e37b94fe3abb42874c319b1c58982e4a9489
jibaru <xxnacho1997xx@gmail.com>
2021-11-15 23:34:52 -0500

    Creado RepresentanteDAO

---
+++ src/main/java/com/untels/zenidscrum/modelo/dao/RepresentanteDAO.java
@@ -0,0 +1,10 @@
+package com.untels.zenidscrum.modelo.dao;
+
+import com.untels.zenidscrum.modelo.bean.Representante;
+
+public interface RepresentanteDAO {
+
+    public boolean crear(Representante r);
+
+    public boolean modificar(Representante r);
+}
